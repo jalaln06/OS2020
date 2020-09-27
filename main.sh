cb01956c0f8ce371085cc959e94ea2db3c7b6b4e
jalaln06 <jalal.naghiyev.06@gmail.com>
2020-09-27 07:54:37 +0300

    added first sign of multi libraring

--- main.sh
+++ main.sh
@@ -1,1 +1,22 @@
-echo fuck you
+sum(){
+    let output=$1+$2
+    echo $1 +  $2 = $output
+    echo $output
+}
+calculate(){
+    case $1 in 
+    "sum") echo "begin sum"
+    sum $2 $3
+    ;;
+    esac
+}
+echo "Start script"
+case $1 in 
+    "calc") echo "calculation start."
+    calculate $2 $3 $4
+    ;;
+    "search") echo "search start"
+    ;;
+    "reverse") echo "reverse start"
+    ;;
+esac
