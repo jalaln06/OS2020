62a0706f0257335f1e454d06a0d3fa8627511c03
jalaln06 <jalal.naghiyev.06@gmail.com>
2020-09-27 07:56:40 +0300

    added library

---
+++ import_lib.sh
@@ -0,0 +1,47 @@
+check_files_exist(){
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/search
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+FILE=/libs/calc
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
+
+}
